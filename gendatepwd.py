4307c29fd93372a342c858be89ae6ad76c53adb9
tomi <tomi.koski@gmail.com>
2020-06-11 21:56:08 +0300

    better

--- gendatepwd.py
+++ gendatepwd.py
@@ -4,19 +4,17 @@
 import calendar
 import locale
 import argparse
+import sys
 
 years = [2020,2019,2018,2017]
 months = []
 
 parser = argparse.ArgumentParser(description="Generate fancy password using dates and stuff")
-parser.add_argument('locale', metavar='LOCALE', nargs='+',help="Locale such as: fi_FI or en_US...")
+parser.add_argument('locale', nargs='+', help="Locale such as: fi_FI or en_US...")
 args = parser.parse_args()
 
-# if you want these in certain language...
-#locale.setlocale(locale.LC_ALL, 'fi_FI')
-
 for l in args.locale:
-
+  locale.setlocale(locale.LC_ALL, l)
   for i in range(1,13):
     months.append(calendar.month_abbr[i])
     months.append(calendar.month_abbr[i].lower())
@@ -26,8 +24,8 @@ for l in args.locale:
     months.append(calendar.month_name[i].upper())
 
 
-  for y in years:
-    for m in months:
-       print(f"{y}{m}")
-       print(f"{m}{y}")
+for y in years:
+  for m in months:
+    print(f"{y}{m}")
+    print(f"{m}{y}")
 
