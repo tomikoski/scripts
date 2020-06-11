07c3a0ce5f2d8224c7837de60a5ef323d5b68993
Tomi (tk0) Koski <1329786+tomikoski@users.noreply.github.com>
2020-06-11 21:58:51 +0300

    error handling

--- gendatepwd.py
+++ gendatepwd.py
@@ -14,18 +14,21 @@ parser.add_argument('locale', nargs='+', help="Locale such as: fi_FI or en_US...
 args = parser.parse_args()
 
 for l in args.locale:
-  locale.setlocale(locale.LC_ALL, l)
-  for i in range(1,13):
-    months.append(calendar.month_abbr[i])
-    months.append(calendar.month_abbr[i].lower())
-    months.append(calendar.month_abbr[i].upper())
-    months.append(calendar.month_name[i])
-    months.append(calendar.month_name[i].lower())
-    months.append(calendar.month_name[i].upper())
+  try:
+    locale.setlocale(locale.LC_ALL, l)
+    for i in range(1,13):
+      months.append(calendar.month_abbr[i])
+      months.append(calendar.month_abbr[i].lower())
+      months.append(calendar.month_abbr[i].upper())
+      months.append(calendar.month_name[i])
+      months.append(calendar.month_name[i].lower())
+      months.append(calendar.month_name[i].upper())
 
+    for y in years:
+     for m in months:
+       print(f"{y}{m}")
+       print(f"{m}{y}")
 
-for y in years:
-  for m in months:
-    print(f"{y}{m}")
-    print(f"{m}{y}")
+  except:
+   print("error: probably incorrect locale?")
 
