95eb4eeefceb9c9f5e8f0f0de393e8685acf83c8
Tomi (tk0) Koski <1329786+tomikoski@users.noreply.github.com>
2024-09-19 20:06:22 +0300

    formatting

--- angr/solve_IOLI_0x00.py
+++ angr/solve_IOLI_0x00.py
@@ -1,21 +1,7 @@
 import angr
 
-def getFuncAddress( funcName, plt=None ):
-    found = [
-        addr for addr,func in cfg.kb.functions.items()
-        if funcName == func.name and (plt is None or func.is_plt == plt)
-        ]
-    if len( found ) > 0:
-        print("Found "+funcName+"'s address at "+hex(found[0])+"!")
-        return found[0]
-    else:
-        raise Exception("No address found for function : "+funcName)
-
 proj = angr.Project("IOLI/bin-linux/crackme0x00", auto_load_libs=False)
 cfg = proj.analyses.CFG()
-addr = getFuncAddress("main")
-
-print(f"Main: {hex(addr)}")
 
 FIND = 0x08048480
 AVOID = 0x08048472
